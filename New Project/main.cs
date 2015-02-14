0acc2da6c7dadda0403e951d965d819b15b33339
JaydenMajor <jaydenmajor@gmail.com>
2015-02-15 08:51:59 +1000

    Created by Coding Ground Client

---
+++ New Project/main.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using System;
+using System.util.generic;
+
+class Program
+{
+    static void Main()
+    {
+        void CheckEmail(string emailAddress,string password,string server,bool imap){
+            // If imap is false use pop3
+            if(imap){
+                
+            }
+            else{
+                
+            }
+        }
+        void GotoPreferenies(string location){
+            suitch(location){
+                case "accounts"
+                break;
+                case "compose":
+                break;
+                case "sendFeedback":
+                break;
+                case "folders":
+                break;
+                case "filters":
+                break;
+                case "about"
+                break;
+                default:
+                break;
+            }
+        }
+    }
+}
